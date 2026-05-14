677e74a875b2a67dbd1ac4552b022a5d9b1f3d40
dma-dojcar <petr.dojcar@proton.me>
2026-05-14 12:11:30 +0000

    add reset passwd file

--- src/reservation.php
+++ src/reservation.php
@@ -91,6 +91,7 @@ $casy = ['08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00'
         <a href="admin.php"><i class="fa-solid fa-gear"></i> Admin</a>
     <?php endif; ?>
     <a href="logout.php"><i class="fa-solid fa-right-from-bracket"></i> Odhlásit se</a>
+    <a href="reset_passwd.php"><i class="fa-solid fa-right-from-bracket"></i> Resetovat heslo</a>
 </nav>
 
 <div class="container">
